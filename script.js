20dc9156adf63e949f6e0635c3a3731ece1645d4
hkeidenb <hasani.eidenberger@gmail.com>
2025-05-05 12:50:05 -0400

    Random variable and if statement

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 
 // Function to get the Computer's Choice
 function getComputerChoice(){
-   let computerChoice = Math.random() * 3;
-   console.log(computerChoice);
+   let computerChoice = Math.random() * 3
+
+if (computerChoice <= .33){
+    console.log("rock");
+    return "rock"
+
+}else if (computerChoice <= .66) {
+    console.log("paper");
+    return "paper"
 }
-getComputerChoice();
+else {
+    console.log("scissors")
+    return "scissors"
+}
+}
+getComputerChoice()
