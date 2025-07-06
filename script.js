0a62c0a3743f29820533c424b7b24e20897986ff
hkeidenb <hasani.eidenberger@gmail.com>
2025-07-05 19:59:45 -0500

    debugged

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
+// Establish variables for Rock, Paper, Scissors"
 
-// Function to get the Computer's Choice
+const choices = ["rock", "paper", "scissors"]
+
+// Function to get the Computer's Choice//
+
 function getComputerChoice(){
-   let computerChoice = Math.random() * 3
+   let computerChoice = Math.random() * 3;
 
-if (computerChoice <= .33){
-    console.log("rock");
-    return "rock"
-
-}else if (computerChoice <= .66) {
-    console.log("paper");
-    return "paper"
-}
-else {
-    console.log("scissors")
-    return "scissors"
-}
+    if (computerChoice <= .33){
+        console.log("rock");
+        return "rock";
+    } else if (computerChoice <= .66) {
+        console.log("paper");
+        return "paper";
+    } else {
+        console.log("scissors")
+        return "scissors";
+    }
 }
 getComputerChoice();
-
 //Function to prompt for User's choice
 function getUserChoice() {
-    let userChoice = prompt("Select either rock, paper, or scissors to play")
+    let userChoice = prompt("Select either 'rock', 'paper', or 'scissors' to play");
     console.log(userChoice);
-    return;
+    return userChoice;
 }
-getUserChoice();
-
 // Compare User Choice to Computer Choice to get a winner
 
 function getWinner (computerChoice,userChoice){
     if (computerChoice === userChoice){
-    console.log("Its a tie");
-    return;
-}
-    if (
-    (computerChoice === "scissors" && userChoice === "paper") ||
-    (computerChoice === "paper" && userChoice === "rock") ||
-    (computerChoice === "rock" && userChoice === "scissors") 
-    ) {
+        console.log("Its a tie");
+        return;
+    } else if 
+        (computerChoice === "scissors" && userChoice === "paper") ||
+        (computerChoice === "paper" && userChoice === "rock") ||
+        (computerChoice === "rock" && userChoice === "scissors") {
         console.log("you Lose");
         return;
+    } else {
+        console.log("you win");
+        return;
+    }
 }
-    else {
-    console.log("you win");
-    return;
-}}
 
-getWinner(computerChoice,userChoice);
+const userChoice = getUserChoice();
+const computerChoice= getComputerChoice();
+getWinner(computerChoice,userChoice)
